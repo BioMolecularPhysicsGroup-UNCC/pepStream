ba78003e4cfcf434b6e4a763ad3900029d3b873d
johnnyp117 <35807919+johnnyp117@users.noreply.github.com>
2022-07-04 09:20:41 -0400

    Delete summarize_zdock.sh

--- summarize_zdock.sh
+++
@@ -1,9 +0,0 @@
-peptide=M-P10
-echo "Summary of model Zdock binding" > ${peptide}_summary.out
-for output in *.out
-do
-
-echo ${output} >> ${peptide}_summary.out
-sed -n '6,10p' ${output} >> ${peptide}_summary.out
-
-done
